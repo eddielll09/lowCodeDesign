85bb6968f3f6a88b96d6a9126aa75c841e11a1c0
liangbh <liangbh@kingsware.cn>
2023-04-04 09:26:50 +0800

    feature: create project

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const state = {
+  num: 1
+};
+
+const mutations = {
+  addNum(state, msg) {
+    state.num = msg++;
+  }
+};
+
+const actions = {};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+});
